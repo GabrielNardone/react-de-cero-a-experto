c64ee908666b820e937ce9b6e641bd860cfecc7a
gabrielNardone <gabriel.f.nardone@gmail.com>
2023-05-30 09:17:22 -0300

    small fix

--- redux/src/api/pokemonApi.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-//* Esto me permite crear una configuración estándar para no estar repitiendo el código
-export const pokemonApi = axios.create({
-    baseURL: `https://pokeapi.co/api/v2`
-})
